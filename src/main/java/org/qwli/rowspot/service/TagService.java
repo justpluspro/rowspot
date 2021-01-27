dbfd1497cc4bd21b5f6f358147734f5f625f0d1a
777 <228794754@qq.com>
2021-01-27 12:51:07 +0800

    Update TagService.java

--- src/main/java/org/qwli/rowspot/service/TagService.java
+++ src/main/java/org/qwli/rowspot/service/TagService.java
@@ -1,8 +1,30 @@
 package org.qwli.rowspot.service;
 
 
+import org.qwli.rowspot.exception.BizException;
+import org.qwli.rowspot.model.Tag;
+import org.qwli.rowspot.repository.TagRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author qwli7
+ * TagService 业务
+ */
 @Service
 public class TagService extends AbstractService<Tag, Tag> {
-  
 
+
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = BizException.class)
+    public void save(Tag tag) {
+        tagRepository.save(tag);
+    }
 }
