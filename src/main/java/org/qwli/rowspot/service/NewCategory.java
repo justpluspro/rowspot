89708b3a5c3324c3538b34feebaedf83ace887e5
777 <228794754@qq.com>
2021-01-27 08:15:19 +0800

    Update NewCategory.java

--- src/main/java/org/qwli/rowspot/service/NewCategory.java
+++ src/main/java/org/qwli/rowspot/service/NewCategory.java
@@ -2,12 +2,25 @@ package org.qwli.rowspot.service;
 
 import java.io.Serializable;
 
+/**
+ * @author qwli7
+ * 新分类
+ */
 public class NewCategory implements Serializable {
 
+    /**
+     *  分类名称
+     */
     private String name;
 
+    /**
+     * 分类别名
+     */
     private String alias;
 
+    /**
+     * 上一级分类 id
+     */
     private Integer parentId;
 
     public String getName() {
