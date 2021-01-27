e66493061c88d82776c9143018bbf56da73e0b64
777 <228794754@qq.com>
2021-01-27 08:24:52 +0800

    Update CategoryFactory.java

--- src/main/java/org/qwli/rowspot/model/factory/CategoryFactory.java
+++ src/main/java/org/qwli/rowspot/model/factory/CategoryFactory.java
@@ -3,18 +3,25 @@ package org.qwli.rowspot.model.factory;
 import org.qwli.rowspot.service.NewCategory;
 import org.qwli.rowspot.model.Category;
 
+/**
+ * @author qwli7
+ * 分类工厂
+ */
 public class CategoryFactory {
 
+    /**
+     * 创建一个 Category
+     * @param newCategory newCategory
+     * @return Category
+     */
     public static Category createCategory(NewCategory newCategory) {
         final Category category = new Category();
-        final Integer parentId = newCategory.getParentId();
+        final Long parentId = newCategory.getParentId();
         if(parentId != null && parentId > 0) {
             category.setParentId(parentId);
         }
-
         category.setName(newCategory.getName());
         category.setAlias(newCategory.getAlias());
-
         return category;
     }
 }
