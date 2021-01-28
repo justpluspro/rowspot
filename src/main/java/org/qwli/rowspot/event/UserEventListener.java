56d5201c137b973225b4fbd0d39c022f84bd0701
花裤衩 <228794754@qq.com>
2021-01-28 08:08:41 +0800

    Update UserEventListener.java

--- src/main/java/org/qwli/rowspot/event/UserEventListener.java
+++ src/main/java/org/qwli/rowspot/event/UserEventListener.java
@@ -6,10 +6,14 @@ import org.qwli.rowspot.model.UserAddition;
 import org.qwli.rowspot.model.factory.UserFactory;
 import org.qwli.rowspot.repository.UserAdditionRepository;
 import org.qwli.rowspot.repository.UserRepository;
+import org.qwli.rowspot.service.processor.EmailBean;
+import org.qwli.rowspot.service.processor.MailProcessor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 
 /**
@@ -35,11 +39,15 @@ public class UserEventListener {
      */
     private final DefaultUserProperties defaultUserProperties;
 
+    private final MailProcessor mailProcessor;
+
     public UserEventListener(UserRepository userRepository,UserAdditionRepository userAdditionRepository,
-                             DefaultUserProperties defaultUserProperties) {
+                             DefaultUserProperties defaultUserProperties,
+                             MailProcessor mailProcessor) {
         this.userRepository = userRepository;
         this.defaultUserProperties = defaultUserProperties;
         this.userAdditionRepository = userAdditionRepository;
+        this.mailProcessor = mailProcessor;
     }
 
     @EventListener(classes = UserLoginEvent.class)
@@ -49,11 +57,28 @@ public class UserEventListener {
         userRepository.updateUserLoginDate(user.getId(), new Date());
     }
 
+    /**
+     * 用户注册事件
+     * 1、添加附属额外信息
+     * 2、发送注册激活邮件
+     * @param event event
+     */
     @EventListener(classes = UserRegisterEvent.class)
     public void processUserRegisterEvent(UserRegisterEvent event) {
         Assert.notNull(event, "UserRegisterEvent not null.");
         final User user = event.getUser();
         UserAddition userAddition = UserFactory.createUserAddition(user, defaultUserProperties);
         userAdditionRepository.save(userAddition);
+        //发送激活邮箱
+        EmailBean emailBean = new EmailBean();
+
+        String mailContent = "<h1>欢迎注册</h1>" +
+                "<p>感谢注册xxx服务，请请点击<a href=''>这里</a>确认激活</p>";
+        emailBean.setContent(mailContent);
+        emailBean.setSubject("Rowspot 激活");
+        emailBean.setHtml(true);
+        emailBean.setSendTos(Collections.singletonList(user.getEmail()));
+        emailBean.setFrom("");
+        mailProcessor.sendHtmlMail(emailBean);
     }
 }
