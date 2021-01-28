9aff3042ede13436a7da071f76d3dad1ee56ccb9
花裤衩 <228794754@qq.com>
2021-01-29 07:49:31 +0800

    Update ArticleQueryParam.java

--- src/main/java/org/qwli/rowspot/web/ArticleQueryParam.java
+++ src/main/java/org/qwli/rowspot/web/ArticleQueryParam.java
@@ -4,24 +4,35 @@ import org.qwli.rowspot.model.enums.ArticleType;
 import org.qwli.rowspot.model.enums.ArticleState;
 
 import java.io.Serializable;
-import java.util.List;
 
-public class ArticleQueryParam implements Serializable {
+/**
+ * @author qwli7
+ * 查询参数
+ */
+public class ArticleQueryParam extends AbstractQueryParam implements Serializable {
 
-    private String userId;
-
-    private Integer page;
+    private Long userId;
 
     private ArticleType articleType;
 
-    private List<ArticleState> states;
+    private ArticleState articleState;
 
-    public List<ArticleState> getStates() {
-        return states;
+    private Long categoryId;
+
+    public Long getCategoryId() {
+        return categoryId;
     }
 
-    public void setStates(List<ArticleState> states) {
-        this.states = states;
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public ArticleState getArticleState() {
+        return articleState;
+    }
+
+    public void setArticleState(ArticleState articleState) {
+        this.articleState = articleState;
     }
 
     public ArticleType getArticleType() {
@@ -32,19 +43,11 @@ public class ArticleQueryParam implements Serializable {
         this.articleType = articleType;
     }
 
-    public String getUserId() {
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
     }
 }
