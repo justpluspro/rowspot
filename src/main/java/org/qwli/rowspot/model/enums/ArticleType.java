ee9c051abd13eaa6dc4ff9a6b77ea320392f6b9c
花裤衩 <228794754@qq.com>
2021-01-29 08:23:39 +0800

    Update ArticleType.java

--- src/main/java/org/qwli/rowspot/model/enums/ArticleType.java
+++ src/main/java/org/qwli/rowspot/model/enums/ArticleType.java
@@ -1,24 +1,59 @@
 package org.qwli.rowspot.model.enums;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author qwli7
+ */
 
 public enum ArticleType {
-    A("article", 0),
-    Q("question", 1),
-    N("news", 2),
-    T("tools", 3),
-    I("index_unique", 0);
+    /**
+     * 文章类型
+     */
+    A("index", "教程",0),
+
+    /**
+     * 问答
+     */
+    Q("issues", "问答区", 1),
+
+    /**
+     * 动态
+     */
+    N("news", "最新动态",2),
+
+    /**
+     * 工具类
+     */
+    T("tools", "实用工具", 3),
+
+    /**
+     * 首页唯一
+     */
+    I("index_unique", "",-1);
 
     String code;
+    String name;
     Integer sort;
 
-    ArticleType(String code, Integer sort) {
+    ArticleType(String code, String name, Integer sort) {
         this.code = code;
         this.sort = sort;
+        this.name = name;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
 
     public static boolean isArticle(String name) {
         return A.name().equals(name);
@@ -44,11 +79,12 @@ public enum ArticleType {
      * 获取所有的文章类型，并排序
      **/
     public List<Map<String, Object>> findAll() {
-        for(ArticleType type : ArticleType.values()) {
-            
-        }
-        
-        return new ArrayList();
-    }
-    
+        ArticleType[] values = ArticleType.values();
+        return Arrays.stream(values).filter(e -> e.getSort() >= 0).sorted(Comparator.comparingInt(ArticleType::getSort)).map(e -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", e.getName());
+            map.put("url", e.getCode());
+            return map;
+        }).collect(Collectors.toList());
+    }   
 }
