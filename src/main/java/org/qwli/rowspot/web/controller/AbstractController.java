62b43755374bd28ca2d6ef48364336e2d5415db3
花裤衩 <228794754@qq.com>
2021-01-29 08:34:22 +0800

    Create AbstractController.java

---
+++ src/main/java/org/qwli/rowspot/web/controller/AbstractController.java
@@ -0,0 +1,27 @@
+package org.qwli.rowspot.web.controller;
+
+import org.qwli.rowspot.exception.ResourceNotFoundException;
+import org.qwli.rowspot.model.Category;
+import org.qwli.rowspot.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author qwli7
+ * @date 2021/1/29 8:29
+ * 功能：AbstractController
+ **/
+public abstract class AbstractController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    private final CategoryService categoryService;
+
+    public AbstractController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    protected Category checkCategory(String alias) {
+       return categoryService.findOne(alias).orElseThrow(()
+               -> new ResourceNotFoundException("category.notExists"));
+    }
+}
