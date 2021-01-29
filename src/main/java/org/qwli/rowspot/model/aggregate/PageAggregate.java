fd5921cc59ebe7debc95517b6e69405e517a7a05
花裤衩 <228794754@qq.com>
2021-01-29 08:14:28 +0800

    Update PageAggregate.java

--- src/main/java/org/qwli/rowspot/model/aggregate/PageAggregate.java
+++ src/main/java/org/qwli/rowspot/model/aggregate/PageAggregate.java
@@ -1,19 +1,86 @@
 package org.qwli.rowspot.model.aggregate;
 
-import org.springframework.data.domain.Page;
-
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * @author qwli7
+ * 分页对象
+ */
 public class PageAggregate<T> implements Serializable {
 
+    /**
+     * 当前页
+     */
     private Integer page;
 
+    /**
+     * 当前页大小
+     */
     private Integer size;
 
+    /**
+     * 数据集合
+     */
     private List<T> data;
 
+    /**
+     * 总页码
+     */
     private Integer totalPage;
+
+    /**
+     * 是否是最后一页
+     */
+    private boolean lastPage;
+
+    /**
+     * 是否是第一页
+     */
+    private boolean firstPage;
+
+    /**
+     * 是否有下一页
+     */
+    private boolean hasNextPage;
+
+    /**
+     * 是否有上一页
+     */
+    private boolean hasPrePage;
+
+
+    public boolean isHasPrePage() {
+        return hasPrePage;
+    }
+
+    public void setHasPrePage(boolean hasPrePage) {
+        this.hasPrePage = hasPrePage;
+    }
+
+    public boolean isLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(boolean lastPage) {
+        this.lastPage = lastPage;
+    }
+
+    public boolean isFirstPage() {
+        return firstPage;
+    }
+
+    public void setFirstPage(boolean firstPage) {
+        this.firstPage = firstPage;
+    }
+
+    public boolean isHasNextPage() {
+        return hasNextPage;
+    }
+
+    public void setHasNextPage(boolean hasNextPage) {
+        this.hasNextPage = hasNextPage;
+    }
 
     public Integer getTotalPage() {
         return totalPage;
@@ -27,6 +94,10 @@ public class PageAggregate<T> implements Serializable {
         this.data = data;
         this.page = number;
         this.totalPage = totalPage;
+        this.firstPage = getPage() == 1;
+        this.lastPage = getPage().equals(getTotalPage());
+        this.hasPrePage = !isFirstPage();
+        this.hasNextPage = !isLastPage();
     }
 
     public PageAggregate(List<T> data, int number, int size, int totalPage) {
@@ -34,6 +105,10 @@ public class PageAggregate<T> implements Serializable {
         this.size = size;
         this.page = number;
         this.totalPage = totalPage;
+        this.firstPage = getPage() == 1;
+        this.lastPage = getPage().equals(getTotalPage());
+        this.hasPrePage = !isFirstPage();
+        this.hasNextPage = !isLastPage();
     }
 
     public Integer getPage() {
