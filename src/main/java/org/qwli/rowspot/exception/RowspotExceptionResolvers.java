845f42a8ae84c748c6a793dc601e90739e48c88b
花裤衩 <228794754@qq.com>
2021-02-01 14:15:56 +0800

    Update RowspotExceptionResolvers.java

--- src/main/java/org/qwli/rowspot/exception/RowspotExceptionResolvers.java
+++ src/main/java/org/qwli/rowspot/exception/RowspotExceptionResolvers.java
@@ -3,7 +3,12 @@ package org.qwli.rowspot.exception;
 import org.qwli.rowspot.exception.reader.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,17 +16,14 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * @author qwli7
  * @date 2021/1/28 8:14
  * 功能：RowspotExceptionResolvers
  **/
-public class RowspotExceptionResolvers implements HandlerExceptionResolver {
+public class RowspotExceptionResolvers implements HandlerExceptionResolver, ErrorAttributes {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
@@ -56,7 +58,13 @@ public class RowspotExceptionResolvers implements HandlerExceptionResolver {
         return null;
     }
 
-
+    /**
+     * resolve exception
+     * @param request request
+     * @param response response
+     * @param ex ex
+     * @return boolean
+     */
     private boolean resolve(HttpServletRequest request, HttpServletResponse response, Exception ex) {
         request.removeAttribute(ERROR_ATTRIBUTES);
 
@@ -92,5 +100,26 @@ public class RowspotExceptionResolvers implements HandlerExceptionResolver {
             }
         }
         return HttpStatus.INTERNAL_SERVER_ERROR.value();
+    }
+
+
+    /**
+     * reader errors from request use 'ERROR_ATTRIBUTES'
+     * @param webRequest webRequest
+     * @param options options
+     * @return Map
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        return (Map<String, Object>) webRequest.getAttribute(ERROR_ATTRIBUTES, RequestAttributes.SCOPE_REQUEST);
+    }
+
+    
+    
+    @Override
+    public Throwable getError(WebRequest webRequest) {
+        
+        return null;
     }
 }
