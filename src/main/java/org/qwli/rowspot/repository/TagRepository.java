4be68cc36a6c77b4266bbbdff9009dce4738ed3b
花裤衩 <228794754@qq.com>
2021-02-04 08:45:56 +0800

    Update TagRepository.java

--- src/main/java/org/qwli/rowspot/repository/TagRepository.java
+++ src/main/java/org/qwli/rowspot/repository/TagRepository.java
@@ -6,7 +6,26 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+/**
+ * @author qwli7
+ */
 @Repository
 public interface TagRepository extends PagingAndSortingRepository<Tag, Long>, QueryByExampleExecutor<Tag> {
+
+    /**
+     * find tag by name
+     * @param name name
+     * @return Tag
+     */
+    Optional<Tag> findByName(String name);
+
+    /**
+     * find tag by alias
+     * @param alias alias
+     * @return Tag
+     */
+    Optional<Tag> findByAlias(String alias);
 
 }
